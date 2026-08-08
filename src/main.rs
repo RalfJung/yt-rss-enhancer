8a3528bb1cd959e981702404d7ddeb28c042ae6e
Ralf Jung <post@ralfj.de>
2026-08-08 11:57:14 +0200

    put more error details into log

--- src/main.rs
+++ src/main.rs
@@ -212,7 +212,7 @@ fn main() -> Result<()> {
             url => Ok(Response::text(format!("endpoint not found: {url}\n")).with_status_code(404)),
         };
         response.unwrap_or_else(|err| {
-            eprintln!("error handling {}: {err}", request.url());
+            eprintln!("error handling {request:?}: {err:?}");
             Response::text(format!("internal server error: {err}\n")).with_status_code(500)
         })
     });
